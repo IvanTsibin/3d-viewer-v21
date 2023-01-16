259645d7b53ab12d6d03b0d9e0d1f5c80e951179
hcarolee <hcarolee@student.21-school.ru>
2023-01-17 00:59:04 +0700

    saved for safe

--- ColorWidgetTest/ColorWidgetTest/mainwindow.cpp
+++ ColorWidgetTest/ColorWidgetTest/mainwindow.cpp
@@ -36,6 +36,14 @@ void MainWindow::on_PBVertexColor_clicked()
     current_color_.setRedF(0.9);
     std::cout << "Ther Red component of the color is int and = " << current_color_.red() << std::endl;
     std::cout << "Ther Red component of the color is QReal and = " << current_color_.redF() << std::endl;
+    FILE *start_pos;
+    start_pos = fopen("start_pos-v3.pos", "w");
+    fprintf(start_pos, "%.2lf ", current_color_.redF());
+    fprintf(start_pos, " %s ", current_color_.name());
+    fclose(start_pos);
+    current_color_.setRgbF(0.6,0.6,0.6);
+    std::cout << "Ther Red component of the color is int and = " << current_color_.red() << std::endl;
+    std::cout << "Ther Red component of the color is QReal and = " << current_color_.redF() << std::endl;
 }
 
 QColor MainWindow::GetColor(void) {
